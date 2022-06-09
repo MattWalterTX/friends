e04022258492abe8512c52f4907e06a318d2337e
Matt Walter <matt.walter.tx@outlook.com>
2022-06-08 22:48:18 -0500

    Add new friends

---
+++ friends.js
@@ -0,0 +1,10 @@
+
+
+var bestFriends = ["Joshua", "Carlos"]
+
+if (bestFriends === "Joshua") {
+  console.log("You are the Best!");
+} else if (bestFriends !== "Carlos") {console.log("You are even Better!");
+}
+else {console.log("You are also the Best!");
+}
